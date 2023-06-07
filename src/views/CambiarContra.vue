9420f6566fc64daae5b22212758df5390860ced7
andrea3309 <amonet3309@gmail.com>
2023-06-06 20:26:13 -0500

    Andrea- Actualización Vista Usuario/Cambiar Contra

--- src/views/CambiarContra.vue
+++ src/views/CambiarContra.vue
@@ -1,21 +1,88 @@
 <template>
   <div class="container">
     <h1>Cambiar Contraseña</h1>
-    <form class="form">
+    <form class="form" @submit="saveChanges">
       <label for="password">Contraseña Actual:</label>
-      <input type="password" id="password" required>
+      <input type="password" id="password" v-model="currentPassword" required>
       <label for="newPassword">Nueva Contraseña:</label>
-      <input type="password" id="newPassword" required>
+      <input type="password" id="newPassword" v-model="newPassword" required>
       <label for="confirmPassword">Confirmar Nueva Contraseña:</label>
-      <input type="password" id="confirmPassword" required>
+      <input type="password" id="confirmPassword" v-model="confirmPassword" required>
       <button type="submit">Guardar Cambios</button>
     </form>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'cambiarcontra'
+  name: 'cambiarcontra',
+  data() {
+    return {
+      cliente:{
+        nombre: 'Andrea',
+        apellido: 'Hidalgo',
+        email: 'andreaa@ejemplo.com',
+        numero: '934984355',
+        provincia: 'Lima',
+        distrito: 'Lima',
+        calle: 'Jr. San Pedro',
+        numcalle: '1309',
+        casadep: 'Casa',
+        recibepedidos: 'Yo mismo',
+      },
+
+      currentPassword: '',
+      newPassword: '',
+      confirmPassword: '',
+    };
+  },
+  methods: {
+    saveChanges() {
+      if (this.newPassword !== this.confirmPassword) {
+        alert('Las contraseñas no coinciden.');
+        return;
+      }
+      
+      if (this.currentPassword === this.newPassword) {
+        alert('La nueva contraseña no puede ser igual a la contraseña actual.');
+        
+      }
+      
+      if (this.currentPassword.length === 0 || this.newPassword.length === 0 || this.confirmPassword.length === 0) {
+        alert('Por favor, completa todos los campos.');
+        return;
+      }
+      
+      // ingresar codigo para guardar en base de datos
+      
+      if(todoestacorrecto){
+        axios.put(this.$url+'/cambiarContraseña',this.$params.id,this.cliente,{
+
+        }).then()
+      }
+      // this.mensaje=result.data.messagge
+      
+      alert('Contraseña cambiada exitosamente.');
+    },
+    initDataUser(){
+      axios.get(this.$url+'/getClienteById'+this.$params.id,this.cliente,{
+        Headers:{
+          'content-Type':'string/Json',
+        'Authorization': this.token
+        }
+      }).then(
+        (result)=> {
+          console.log(result.data)
+          this.cliente=result.data
+      }).catch((err)=> {return console.log(err)})
+    }
+    
+  },
+  beforeMount(){
+    this.initDataUser()
+  }
 };
 </script>
 
--- src/views/VistaUsuario.vue
+++ src/views/VistaUsuario.vue
@@ -1,14 +1,77 @@
 <template>
   <div class="container">
-    <h1>Perfil de Usuario</h1>
+    <h1></h1>
     <div class="user-info">
-      <h2>Información Personal</h2>
-      <p><strong>Nombre:</strong> {{ user.firstName }}</p>
-      <p><strong>Apellido:</strong> {{ user.lastName }}</p>
-      <p><strong>Correo Electrónico:</strong> {{ user.email }}</p>
-      <p><strong>Número:</strong> {{ user.number }}</p>
+      <h2>INFORMACIÓN PERSONAL</h2>
+      <table>
+        <tr>
+          <td><strong>Nombre:</strong></td>
+          <td>
+            <input v-model="user.nombre" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Apellido:</strong></td>
+          <td>
+            <input v-model="user.apellido" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Correo Electrónico:</strong></td>
+          <td>
+            <input v-model="user.email" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Número de Contacto:</strong></td>
+          <td>
+            <input v-model="user.numero" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Provincia:</strong></td>
+          <td>
+            <input v-model="user.provincia" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Distrito:</strong></td>
+          <td>
+            <input v-model="user.distrito" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Calle:</strong></td>
+          <td>
+            <input v-model="user.calle" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Número:</strong></td>
+          <td>
+            <input v-model="user.numcalle" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Casa/Depto:</strong></td>
+          <td>
+            <input v-model="user.casadep" :disabled="!editingMode" />
+          </td>
+        </tr>
+        <tr>
+          <td><strong>Recibe Pedido:</strong></td>
+          <td>
+            <input v-model="user.recibepedidos" :disabled="!editingMode" />
+          </td>
+        </tr>
+      </table>
     </div>
-    <button v-on:click="cambiarContra()" @click="modificarContraseña">Modificar Contraseña</button>
+    <div class="button-container">
+      <button @click="toggleEditingMode">
+        {{ editingMode ? 'Guardar Cambios' : 'Editar Información' }}
+      </button>
+      <button v-on:click="cambiarContra()" @click="modificarContraseña">Modificar Contraseña</button>
+    </div>
   </div>
 </template>
 
@@ -18,24 +81,43 @@ export default {
   data() {
     return {
       user: {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        number: '1234567890'
+        nombre: 'Andrea',
+        apellido: 'Hidalgo',
+        email: 'andreaa@ejemplo.com',
+        numero: '934984355',
+        provincia: 'Lima',
+        distrito: 'Lima',
+        calle: 'Jr. San Pedro',
+        numcalle: '1309',
+        casadep: 'Casa',
+        recibepedidos: 'Yo mismo',
       },
+      editingMode: false,
     };
   },
   methods: {
-    cambiarContra(){
+    cambiarContra() {
       this.$router.push('/CambiarContra');
-    }
-  }
+    },
+    toggleEditingMode() {
+      if (this.editingMode) {
+        // Lógica para guardar los cambios
+        // Aquí puedes enviar los datos actualizados al servidor
+        console.log('Guardando cambios:', this.user);
+      }
+      this.editingMode = !this.editingMode;
+    },
+    modificarContraseña() {
+      // Agrega aquí la lógica para modificar la contraseña del usuario
+    },
+  },
 };
 </script>
+
 <style>
 .container {
   display: flex;
-  margin-top: 1rem;
+  margin-top: 11rem;
   flex-direction: column;
   align-items: center;
   justify-content: center;
@@ -60,6 +142,12 @@ export default {
   margin-bottom: 10px;
 }
 
+.button-container {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+
 button {
   padding: 10px 15px;
   background-color: #007bff;
@@ -67,5 +155,28 @@ button {
   border: none;
   border-radius: 4px;
   cursor: pointer;
+}
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+table td,
+table th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+table tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+table th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #007bff;
+  color: white;
 }
 </style>
